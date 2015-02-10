c4c15aa968fc5e424e0ab16e42240ac86c3b5235
Will Morrow <duh102@gmail.com>
2015-02-10 15:49:43 -0500

    Tried to get input working, doesn't

--- maze.js
+++ maze.js
@@ -1,8 +1,9 @@
 var canvas = CE.defines("canvas_id").
+            extend(Input).
             ready(function() {
                 canvas.Scene.call("MyScene");
-            });
-
+            }),
+    thisThing;
 
 canvas.Scene.new({
     name: "MyScene",
@@ -12,15 +13,34 @@ canvas.Scene.new({
         }
     },
     ready: function(stage) {
+		thisThing = this;
+        this.el = this.createElement(32,32);
+        this.el.drawImage("smile");
 
-        var el = this.createElement();
-        el.drawImage("smile");
-
-        el.on("click", function(e) { // or el.click(function(e) {
-            this.opacity = this.opacity < 1 ? 1 : 0.5 ;
+        stage.append(this.el);
+		//don't know why this doesn't work
+		canvas.Input.keyUp(Input.Up, function(e) {
+			if(thisThing.el.y > 0)
+				thisThing.el.y = Math.max(0,thisThing.el.y-32);
         });
 
-        stage.append(el);
+		canvas.Input.keyUp(Input.Bottom, function(e) {
+			if(thisThing.el.y < canvas.height)
+				thisThing.el.y = Math.min(canvas.height-32,thisThing.el.y+32);
+        });
 
+		canvas.Input.keyUp(Input.Left, function(e) {
+			if(thisThing.el.x > 0)
+				thisThing.el.x = Math.max(0,thisThing.el.x-32);
+        });
+
+		canvas.Input.keyUp(Input.Right, function(e) {
+			if(thisThing.el.x > canvas.width)
+				thisThing.el.x = Math.max(canvas.width-32,thisThing.el.x+32);
+        });
+    },
+    render: function(stage) {
+
+        stage.refresh();
     }
 });
