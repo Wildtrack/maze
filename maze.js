a2f6f2c0d75bd2628da2c680e1483816155f3bdf
Will <duh102@gmail.com>
2015-02-10 18:18:46 -0500

    Fixed user input

--- maze.js
+++ maze.js
@@ -16,27 +16,40 @@ canvas.Scene.new({
 		thisThing = this;
         this.el = this.createElement(32,32);
         this.el.drawImage("smile");
+        this.el.x = 32;
+        this.el.y = 32;
+        _canvas = this.getCanvas();
 
         stage.append(this.el);
 		//don't know why this doesn't work
+
+		console.log("width: " + _canvas.width + ", height: " + _canvas.height);
 		canvas.Input.keyUp(Input.Up, function(e) {
 			if(thisThing.el.y > 0)
+			{
 				thisThing.el.y = Math.max(0,thisThing.el.y-32);
+			}
         });
 
 		canvas.Input.keyUp(Input.Bottom, function(e) {
-			if(thisThing.el.y < canvas.height)
-				thisThing.el.y = Math.min(canvas.height-32,thisThing.el.y+32);
+			if(thisThing.el.y < _canvas.height)
+			{
+				thisThing.el.y = Math.min(_canvas.height-32,thisThing.el.y+32);
+			}
         });
 
 		canvas.Input.keyUp(Input.Left, function(e) {
 			if(thisThing.el.x > 0)
+			{
 				thisThing.el.x = Math.max(0,thisThing.el.x-32);
+			}
         });
 
 		canvas.Input.keyUp(Input.Right, function(e) {
-			if(thisThing.el.x > canvas.width)
-				thisThing.el.x = Math.max(canvas.width-32,thisThing.el.x+32);
+			if(thisThing.el.x < _canvas.width)
+			{
+				thisThing.el.x = Math.min(_canvas.width-32,thisThing.el.x+32);
+			}
         });
     },
     render: function(stage) {
