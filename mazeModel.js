c083a992953f6d0c622d39c0d9698dbce836f3f9
Will Morrow <duh102@gmail.com>
2015-02-11 11:56:47 -0500

    expanded accessibility check, bugfix

--- mazeModel.js
+++ mazeModel.js
@@ -1,6 +1,8 @@
 var AMaze = AMaze || {};
 AMaze.model = {
-	var Maze = function(opts){
+	//Maze constructor
+	var N_CONST = 1, E_CONST = 2, S_CONST = 4, W_CONST = 8,
+	Maze = function(opts){
 		//opts is an object with optional params, width and height
 		//default to 10x10 maze
 
@@ -9,7 +11,7 @@ AMaze.model = {
 
 		//default start and end are 0,0 and (width),(height)
 		this.start = [0,0];
-		this.end = [width, height];
+		this.end = [this.width, this.height];
 
 		//board is a 2d array of cells, each cell is one of 16 states
 		//check with bitwise and
@@ -24,13 +26,37 @@ AMaze.model = {
 		//13 & 8 == 8: accessible from w
 
 		this.board = [];
-		for( var y = height; y--; )
+		for( var y = this.height; y--; )
 		{
 			this.board.push([]);
-			for( var x = width; x--; )
+			for( var x = this.width; x--; )
 			{
-				this.board[y][width-x-1] = 0;
+				this.board[y][this.width-x-1] = 0;
 			}
 		}
+	};
+
+	//returns cell-type number that gives you the true possible exits
+	//ie: a wall can only be traveled through if cells on both sides agree
+	//that there is an opening there
+	Maze.prototype.acessibleExits = function(x, y) {
+		var acc = 0;
+		if(y-1 >= 0 && (this.board[x][y] & N_CONST == N_CONST) && (this.board[x][y-1] & S_CONST == S_CONST))
+		{
+			acc+=N_CONST;
+		}
+		if(y+1 < this.height && (this.board[x][y] & S_CONST == S_CONST) && (this.board[x][y+1] & N_CONST == N_CONST))
+		{
+			acc+=S_CONST;
+		}
+		if(x-1 >= 0 && (this.board[x][y] & W_CONST == W_CONST) && (this.board[x-1][y] & E_CONST == E_CONST))
+		{
+			acc+=W_CONST;
+		}
+		if(x+1 < this.width && (this.board[x][y] & E_CONST == E_CONST) && (this.board[x+1][y] & W_CONST == W_CONST))
+		{
+			acc+=E_CONST;
+		}
+		return acc;
 	};
 };
