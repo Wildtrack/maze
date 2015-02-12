4109238fa9878bf7e225246c24416d69138269c6
Will Morrow <duh102@gmail.com>
2015-02-11 23:50:01 -0500

    Fixed global scope of thisThing

--- maze.js
+++ maze.js
@@ -1,9 +1,8 @@
-var canvas = CE.defines("canvas_id").
-            extend(Input).
-            ready(function() {
+var canvas = CE.defines("canvas_id")
+            .extend(Input)
+            .ready(function() {
                 canvas.Scene.call("MyScene");
-            }),
-    thisThing;
+            });
 
 canvas.Scene.new({
     name: "MyScene",
