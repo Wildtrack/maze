e5fd2481a57cd1972c50130a08f7e3f042540a52
Mark Luo <macluo@gmail.com>
2015-02-14 17:14:06 -0500

    Added mechanism to track traildots

--- maze.js
+++ maze.js
@@ -31,11 +31,61 @@ canvas.Scene.new({
             return trail;
         }
 
+        //trail dot
+        trailDot = function (x, y) {
+            trail = thisThing.createElement(16,16);
+            trail.drawImage("trail");
+            trail.x = x;
+            trail.y = y;
+            return trail;
+        };
+
+        var trailModel = {
+        	width: 20, height: 20, theDots: [],
+        	map: function() {
+
+        		//this.theDots = [];
+				for( var x = trailModel.width; x--; )
+				{
+					trailModel.theDots.push([]);
+					for( var y = trailModel.height; y--; )
+					{
+						trailModel.theDots[trailModel.width-x-1].push();
+					}
+				}
+        	}
+        };
+
+        trailModel.map.prototype.exists = function(x, y) 
+        {
+        		var data = trailModel.theDots[x][y];
+        		if (typeof data == "object")
+        		{
+        			//data.remove();
+        			//trailModel.theDots[x][y] = null;
+        			return 1;
+        		}
+        		return 0;
+        };
+
+        trailModel.map.prototype.makeTrail = function(x, y) 
+        {
+        		var x1 = x/32;
+        		var y1 = y/32;
+        		if (!this.exists(x1, y1)) {
+        			var newDot = new trailDot(x, y);
+        			trailModel.theDots[x1][y1] = newDot;
+        			stage.append(newDot);
+        		}
+        };
+
+        var theTrail = new trailModel.map();
+
 		//console.log("width: " + _canvas.width + ", height: " + _canvas.height);
 		canvas.Input.keyUp(Input.Up, function(e) {
 			if(thisThing.el.y > 0)
 			{
-                stage.append(new trailDot(thisThing.el.x, thisThing.el.y));
+                theTrail.makeTrail(thisThing.el.x, thisThing.el.y);
 				thisThing.el.y = Math.max(0,thisThing.el.y-32);
 			}
         });
@@ -43,7 +93,7 @@ canvas.Scene.new({
 		canvas.Input.keyUp(Input.Bottom, function(e) {
 			if(thisThing.el.y < _canvas.height)
 			{
-                stage.append(new trailDot(thisThing.el.x, thisThing.el.y));
+                theTrail.makeTrail(thisThing.el.x, thisThing.el.y);
 				thisThing.el.y = Math.min(_canvas.height-32,thisThing.el.y+32);
 			}
         });
@@ -51,7 +101,7 @@ canvas.Scene.new({
 		canvas.Input.keyUp(Input.Left, function(e) {
 			if(thisThing.el.x > 0)
 			{
-                stage.append(new trailDot(thisThing.el.x, thisThing.el.y));
+                theTrail.makeTrail(thisThing.el.x, thisThing.el.y);
 				thisThing.el.x = Math.max(0,thisThing.el.x-32);
 			}
         });
@@ -59,7 +109,7 @@ canvas.Scene.new({
 		canvas.Input.keyUp(Input.Right, function(e) {
 			if(thisThing.el.x < _canvas.width)
 			{
-                stage.append(new trailDot(thisThing.el.x, thisThing.el.y));
+                theTrail.makeTrail(thisThing.el.x, thisThing.el.y);
 				thisThing.el.x = Math.min(_canvas.width-32,thisThing.el.x+32);
 			}
         });
