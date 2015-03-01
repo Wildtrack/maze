90c46f9282fdd221269f93c32b3e211466400e44
--replace-all <ktspence@ncsu.edu>
2015-03-01 08:45:26 -0500

    Added barebones for canvas attribute and function definition.

--- designer/mazeDesigner.js
+++ designer/mazeDesigner.js
@@ -1,6 +1,20 @@
-
 var canvas = CE.defines("canvas_id")
             .extend(Input)
             .ready(function() {
                 canvas.Scene.call("MyScene");
             });
+
+canvas.Scene.new({
+	name: "MyScene",
+	materials: {
+		images: {
+		
+		}
+	},
+	ready: function(stage) {
+		
+	},
+	render: function(stage) {
+		stage.refresh();
+	}
+}
