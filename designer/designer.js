526e9417e5a687116d90696a9b401efd98f74f9f
--replace-all <ktspence@ncsu.edu>
2015-03-02 09:51:00 -0500

    Added functionality for Set Start tool; made table cells bigger

--- designer/designer.js
+++ designer/designer.js
@@ -1,7 +1,7 @@
 var tool = 0;
 var maze;
 
-var start_x, start_y, finish_x, finish_y;
+var start_cell, finish_cell;
 
 function set_size() {
 	w = document.getElementById('width').value;
@@ -16,17 +16,18 @@ function set_size() {
 		for (x = 0; x < w; x++) {
 			maze[y][x] = [0];
 			table_cell = document.createElement('td');
-			table_cell.width = 20;
-			table_cell.height = 20;
+			table_cell.width = 30;
+			table_cell.height = 30;
 			table_cell.tag_x = x;
 			table_cell.tag_y = y;
 			table_cell.onclick = cell_click;
+			table_cell.align = 'center';
 			table_row.appendChild(table_cell);
 		}
 		mtbl.appendChild(table_row);
 	}
 	
-	start_x = start_y = finish_x = finish_y = 0;
+	start_cell = finish_cell = null;
 }
 
 function set_tool(t) {
@@ -37,13 +38,25 @@ function cell_click() {
 	switch (tool) {
 		case 0:
 			maze[this.tag_y][this.tag_x] = 1 - maze[this.tag_y][this.tag_x];
-			this.style.background = maze[this.tag_y][this.tag_x] ? '#000000' : 'none';
+			update_cell_color(this);
 			break;
 		case 1:
-		
+			if (start_cell != null)
+				start_cell.innerHTML = '';
+			if (start_cell != this) {
+				start_cell = this;
+				this.innerHTML = 'S';
+			} else {
+				start_cell = null;
+			}
 			break;
 		case 2:
 			
 			break;
 	}
+}
+
+function update_cell_color(cell) {
+	cell.style.background = maze[cell.tag_y][cell.tag_x] ? '#000000' : 'none';
+	cell.style.color = maze[cell.tag_y][cell.tag_x] ? '#FFFFFF' : '#000000';
 }
