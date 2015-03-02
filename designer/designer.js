f67b5b8fddd9c581deaecbc46a439168f586764a
--replace-all <ktspence@ncsu.edu>
2015-03-02 09:45:02 -0500

    Enabled path tool

--- designer/designer.js
+++ designer/designer.js
@@ -1,6 +1,8 @@
 var tool = 0;
 var maze;
 
+var start_x, start_y, finish_x, finish_y;
+
 function set_size() {
 	w = document.getElementById('width').value;
 	h = document.getElementById('height').value;
@@ -16,13 +18,15 @@ function set_size() {
 			table_cell = document.createElement('td');
 			table_cell.width = 20;
 			table_cell.height = 20;
-			table_cell.id_x = x;
-			table_cell.id_y = y;
+			table_cell.tag_x = x;
+			table_cell.tag_y = y;
 			table_cell.onclick = cell_click;
 			table_row.appendChild(table_cell);
 		}
 		mtbl.appendChild(table_row);
 	}
+	
+	start_x = start_y = finish_x = finish_y = 0;
 }
 
 function set_tool(t) {
@@ -32,7 +36,8 @@ function set_tool(t) {
 function cell_click() {
 	switch (tool) {
 		case 0:
-		
+			maze[this.tag_y][this.tag_x] = 1 - maze[this.tag_y][this.tag_x];
+			this.style.background = maze[this.tag_y][this.tag_x] ? '#000000' : 'none';
 			break;
 		case 1:
 		
