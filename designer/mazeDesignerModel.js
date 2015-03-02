a196ae3ee0bd5b513dbbc009d117857968fc8d1e
--replace-all <ktspence@ncsu.edu>
2015-03-01 22:11:13 -0500

    Added basic mazeDesignerModel.js

---
+++ designer/mazeDesignerModel.js
@@ -0,0 +1,12 @@
+//Incantation to check whether we are in the browser or node
+if (typeof window === "undefined")
+    var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
+
+var AMaze = AMaze || {};
+var AMaze = AMaze || {};
+AMaze.model = {
+	//Maze Designer constructor
+	Maze: function(opts){
+	
+	}
+};
