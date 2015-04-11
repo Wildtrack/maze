872856d25c16006ff9b5d738c9fc6f347839987d
Matthew Neal <meneal@gmail.com>
2015-04-10 21:38:43 -0400

    setup server.js to run on port 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 app.use(express.static(__dirname + '/public'));
 app.listen(port, function() {
 	console.log("Our app is running on http://localhost:" + port);
